f1acfcedf324520d5f822f4fb8e9c11df108d7e3
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2026-08-01 18:09:49 +0300

    update

--- src/content/docs/modules/description/EKIO-DIx32/options.ts
+++ src/content/docs/modules/description/EKIO-DIx32/options.ts
@@ -4,34 +4,37 @@ import type {
   IOptionVariant,
 } from "../../../../../components/Options.astro";
 
+const voltage: IOptionVariant[] = [
+  { name: "x", desc: "Не устанавливать", price: 777 },
+  { name: "5", desc: "5 В", price: 777 },
+  { name: "9", desc: "9 В", price: 777 },
+  { name: "12", desc: "12 В", price: 777 },
+  { name: "24", desc: "24 В", price: 777 },
+];
+
+
 export const options_config: IOptionsConfig = {
   base_price: 14.8,
   options: [
     {
-      name: "Количество дискретных входов",
-      default: 3,
-      variants: [
-        {
-          name: "8",
-          desc: "8 каналов",
-          price: 0.8,
-        } as IOptionVariant,
-        {
-          name: "16",
-          desc: "16 каналов",
-          price: 1.6,
-        } as IOptionVariant,
-        {
-          name: "24",
-          desc: "24 канала",
-          price: 2.4,
-        } as IOptionVariant,
-        {
-          name: "32",
-          desc: "32 канала",
-          price: 3.2,
-        } as IOptionVariant,
-      ],
+      name: "Напряжение для каналов CH0-CH7",
+      default: 4,
+      variants: voltage,
+    } as IOption,
+    {
+      name: "Напряжение для каналов CH8-CH15",
+      default: 4,
+      variants: voltage,
+    } as IOption,
+    {
+      name: "Напряжение для каналов CH16-CH23",
+      default: 4,
+      variants: voltage,
+    } as IOption,
+    {
+      name: "Напряжение для каналов CH24-CH31",
+      default: 4,
+      variants: voltage,
     } as IOption,
     {
       name: "Схема подключения входов",
@@ -48,27 +51,6 @@ export const options_config: IOptionsConfig = {
         } as IOptionVariant,
       ],
       default: 0,
-    } as IOption,
-    {
-      name: "Коннекторы шины IBus",
-      variants: [
-        {
-          name: "T",
-          desc: "Только сверху (top)",
-          price: 0.1,
-        } as IOptionVariant,
-        {
-          name: "B",
-          desc: "Только снизу (bottom)",
-          price: 0.1,
-        } as IOptionVariant,
-        {
-          name: "TB",
-          desc: "Сверху и снизу (top + bottom)",
-          price: 0.2,
-        } as IOptionVariant,
-      ],
-      default: 2,
     } as IOption,
   ],
 };
